effe50d0cb05b6c80189f6823969d5fa87f85d50
pylot <contact@borasoftware.com>
2019-05-06 17:23:17 +0100

    Bumped version

--- src/main/cmake/BalauConfig.cmake
+++ src/main/cmake/BalauConfig.cmake
@@ -1,4 +1,4 @@
 
 get_filename_component(Balau_INSTALL_PREFIX "${CMAKE_CURRENT_LIST_DIR}/../../" ABSOLUTE)
-set(Balau_INCLUDE_DIRS ${Balau_INSTALL_PREFIX}/include/Balau-2019.3.1)
-set(Balau_LIBRARY ${Balau_INSTALL_PREFIX}/lib/Balau-2019.3.1/libBalau.a)
+set(Balau_INCLUDE_DIRS ${Balau_INSTALL_PREFIX}/include/Balau-2019.5.1)
+set(Balau_LIBRARY ${Balau_INSTALL_PREFIX}/lib/Balau-2019.5.1/libBalau.a)
